348e163a02a98d9e6b2446106d0191cd3774f956
Omercan Celikler <omercan.celikler@ug.bilkent.edu.tr>
2020-11-17 21:49:31 +0300

    commit
    Signed-off-by: Omercan Celikler <omercan.celikler@ug.bilkent.edu.tr>

--- components/profile/index.js
+++ components/profile/index.js
@@ -1,5 +1,4 @@
 import React from "react";
-<<<<<<< HEAD
 import axios from "../../utils/Axios.js";
 import Toast from "react-native-toast-message";
 import BottomBar from "../bottom-bar";
@@ -8,13 +7,6 @@ import { Button, Input, Text, Icon, TopNavigation, List } from "@ui-kitten/compo
 import { StyleSheet, View } from "react-native";
 
 
-=======
-import BottomBar from "../bottom-bar";
-import TopNavigation from "../top-navigation";
-import { Button, Icon, List, ListItem, Text, Avatar } from "@ui-kitten/components";
-import { StyleSheet, View } from "react-native";
-
->>>>>>> 33d8e13a063e7c7b57ec295b000e18987837b41e
 const renderItemAccessory = () => (
 	<Button size="small">Gözat</Button>
 );
@@ -25,23 +17,15 @@ const renderItemIcon = (props) => (
 
 const renderItem = ({ item }) => (
 	<ListItem
-<<<<<<< HEAD
 		title={item.phoneNumber}
 		description={item.fullName}
 		accessoryLeft={renderItemIcon}
 		accessoryRight={renderItemAccessory}
 	
-=======
-		title={item.title}
-		description={item.description}
-		accessoryLeft={renderItemIcon}
-		accessoryRight={renderItemAccessory}
->>>>>>> 33d8e13a063e7c7b57ec295b000e18987837b41e
 	/>
 );
 
 class Profile extends React.PureComponent {
-<<<<<<< HEAD
     constructor() {
 		super();
 		this.state = {
@@ -70,42 +54,12 @@ class Profile extends React.PureComponent {
 			console.log("catch");
 		})
     }
-=======
-	constructor() {
-		super();
-		this.state = {
-		
-			loading: false
-		}
-	}
-
-
-	// requestUserLogin = () => {
-	// 	this.setState({ loading: true }, () => {
-	// 		axios.get("/users-ws/api/user/login ", {
-	// 			email: this.state.email,
-	// 			password: this.state.password
-	// 		})
-	// 		.then(async(response) => {
-	// 			Toast.show("Giriş başarılı!");
-	// 			await AsyncStorage.setItem("@token", response.data.token);
-	// 			this.props.navigation.navigate("List");
-	// 		})
-	// 		.catch((error) => {
-	// 			console.log(error);
-	// 			Toast.show("Kullanıcı adı veya şifre hatalı!");
-	// 		})
-	// 		.finally(() => { this.setState({ loading: false }); });
-	// 	})
-	// }
->>>>>>> 33d8e13a063e7c7b57ec295b000e18987837b41e
 
 
 	render() {
 		return (
 			<>
 				<TopNavigation title="Profil" />
-<<<<<<< HEAD
                     <View style={CSS.input_container}>
                         {/* <Icon fill="#8F9BB3" name="home" style={CSS.icon} /> */}
                         <List
@@ -115,10 +69,6 @@ class Profile extends React.PureComponent {
 
                     </View>
                 <BottomBar />
-=======
-					<Text style={CSS.input__text}>Omercan Celikler</Text>
-				<BottomBar index={2} navigation={this.props.navigation} />
->>>>>>> 33d8e13a063e7c7b57ec295b000e18987837b41e
 			</>
 		);
 	}
@@ -145,12 +95,6 @@ const CSS = StyleSheet.create({
 		marginBottom: "0.50%",
 		fontSize: 14,
 		fontWeight: "700"
-<<<<<<< HEAD
-=======
-	},
-	spinner: {
-		marginTop: "10%"
->>>>>>> 33d8e13a063e7c7b57ec295b000e18987837b41e
 	}
 });
 
